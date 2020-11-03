7941f0547184a8570414301b9c254f4f7e063445
Crimix <christofferm@live.dk>
2020-11-03 15:15:18 +0100

    Fixed MultiMapHelper

--- src/main/java/com/black_dog20/bml/utils/item/MultiMapHelper.java
+++ src/main/java/com/black_dog20/bml/utils/item/MultiMapHelper.java
@@ -1,6 +1,7 @@
 package com.black_dog20.bml.utils.item;
 
 import com.google.common.collect.Multimap;
+import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class MultiMapHelper {
      * @param key      the key (category).
      * @param uuid     the uuid of the attribute to be removed.
      */
-    public static void removeValues(Multimap<String, AttributeModifier> multimap, String key, UUID uuid) {
+    public static void removeValues(Multimap<Attribute, AttributeModifier> multimap, Attribute key, UUID uuid) {
         List<AttributeModifier> list = multimap.get(key).stream()
                 .filter(a -> a.getID().equals(uuid))
                 .collect(Collectors.toList());
