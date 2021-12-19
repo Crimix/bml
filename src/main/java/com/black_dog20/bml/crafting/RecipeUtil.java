e6b4dacc237fe0f6fcc03c9fdf39a1a3cb764d0a
crimix <christofferm@live.dk>
2021-12-19 19:01:20 +0100

    Fixed crashes

--- src/main/java/com/black_dog20/bml/crafting/RecipeUtil.java
+++ src/main/java/com/black_dog20/bml/crafting/RecipeUtil.java
@@ -1,6 +1,5 @@
 package com.black_dog20.bml.crafting;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
@@ -11,10 +10,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.minecraftforge.common.crafting.CraftingHelper;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -25,11 +21,6 @@ import java.util.Map;
  */
 public class RecipeUtil {
 
-    private static final Method DESERIALIZE_KEY = ObfuscationReflectionHelper.findMethod(ShapedRecipe.class, "keyFromJson" /* deserializeKey */, JsonObject.class);
-    private static final Method SHRINK = ObfuscationReflectionHelper.findMethod(ShapedRecipe.class, "shrink" /* shrink */, String[].class);
-    private static final Method PATTERN_FROM_JSON = ObfuscationReflectionHelper.findMethod(ShapedRecipe.class, "patternFromJson" /* patternFromJson*/, JsonArray.class);
-    private static final Method DESERIALIZE_INGREDIENTS = ObfuscationReflectionHelper.findMethod(ShapedRecipe.class, "dissolvePattern" /* deserializeIngredients */, String[].class, Map.class, int.class, int.class);
-
     /**
      * Parse the ingredients of a shaped recipe.
      * <p>
@@ -39,20 +30,13 @@ public class RecipeUtil {
      * @return A primer containing the ingredients, recipe width and recipe height specified in the JSON object.
      */
     public static ShapedPrimer parseShaped(final JsonObject json) {
-        try {
-            @SuppressWarnings("unchecked") final Map<String, Ingredient> key = (Map<String, Ingredient>) DESERIALIZE_KEY.invoke(null, GsonHelper.getAsJsonObject(json, "key"));
+        final Map<String, Ingredient> key = ShapedRecipe.keyFromJson(GsonHelper.getAsJsonObject(json, "key"));
+        final String[] pattern = ShapedRecipe.shrink(ShapedRecipe.patternFromJson(GsonHelper.getAsJsonArray(json, "pattern")));
+        final int recipeWidth = pattern[0].length();
+        final int recipeHeight = pattern.length;
+        final NonNullList<Ingredient> ingredients = ShapedRecipe.dissolvePattern(pattern, key, recipeWidth, recipeHeight);
 
-            final String[] pattern = (String[]) SHRINK.invoke(null, PATTERN_FROM_JSON.invoke(null, GsonHelper.getAsJsonArray(json, "pattern")));
-
-            final int recipeWidth = pattern[0].length();
-            final int recipeHeight = pattern.length;
-
-            @SuppressWarnings("unchecked") final NonNullList<Ingredient> ingredients = (NonNullList<Ingredient>) DESERIALIZE_INGREDIENTS.invoke(null, pattern, key, recipeWidth, recipeHeight);
-
-            return new ShapedPrimer(ingredients, recipeWidth, recipeHeight);
-        } catch (final IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException("Failed to parse shaped recipe", e);
-        }
+        return new ShapedPrimer(ingredients, recipeWidth, recipeHeight);
     }
 
     /**
